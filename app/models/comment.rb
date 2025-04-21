5feb3f9aa19d3449587f4c2f845578f61a7cdb15
rodrigotoledo <rodrigo@rtoledo.inf.br>
2025-04-21 17:54:09 -0300

    Refactor broadcast_update_to method in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   validates :content, :author, presence: true
   after_create_commit do
-    broadcast_update_to [ user.id, "comments" ], target: "comments", partial: "comments/table_of_comments", locals: { comments: Comment.order(created_at: :desc) }
+    broadcast_update_to [ user.id, "comments" ], target: "comments", partial: "comments/table_of_comments", locals: { comments: user.comments.order(created_at: :desc) }
     broadcast_update_to "total_of_comments", target: "total_of_comments", partial: "comments/total_of_comments"
   end
   after_update_commit do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 User.create(email_address: "foo@bar", password: "foo@bar")
 User.create(email_address: "bar@foo", password: "bar@foo")
+1000.times do
+  Comment.create(author: Faker::Book.author, content: Faker::Lorem.sentence, user: User.first)
+  Comment.create(author: Faker::Book.author, content: Faker::Lorem.sentence, user: User.last)
+end
